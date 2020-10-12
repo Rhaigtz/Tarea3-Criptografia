9ce6ccc33bcc98e3cd7f955eb0c1da050dbd1b60
Nicolás Opazo <nopazog@gmail.com>
2020-10-12 20:48:30 -0300

    commit

--- html.py
+++ html.py
@@ -1,30 +1,11 @@
-# write-html-2-windows.py
+from arc4 import ARC4
+from reece4 import Text
+from rece4 import encrypt, decrypt
 
-import webbrowser
-from rijndael.cipher.crypt import new
-from rijndael.cipher.blockcipher import MODE_CBC
+encrypted = encrypt('key', 'text')
 
-
-
-rjn = new('thisisatestkeywhichis32byteslong', MODE_CBC, 'thisisatestkeywhichis32byteslong', 32)
-encd = rjn.encrypt('A padded string to BLOCKSIZE length.')
-
-f = open('helloworld.html','wb')
+print(encrypted)
+decrypted = decrypt('key', encrypted)
 
 
-
-
-message = """<html>
-<head></head>
-<body><p>Este sitio contiene un mensaje secreto</p>
-<div classname={} id={}>
-</body>
-</html>""".format('rijndael', encd)
-
-
-
-
-f.write(message.encode())
-f.close()
-
-webbrowser.open_new_tab('helloworld.html')
+print(decrypted)
