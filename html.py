dad768853c4ca624274fede9459e2b1b7a48f658
Rhaigtz <nopazog@gmail.com>
2020-10-14 22:04:16 -0300

    UPDATE: Se agrega key a los valores del div

--- html.py
+++ html.py
@@ -2,7 +2,7 @@ import webbrowser
 from oscrypto import symmetric
 import codecs
 
-encrypted = symmetric.rc4_encrypt(b'key 123', b'text')
+encrypted = symmetric.rc4_encrypt(b'key123', b'text')
 
 
 f = open('index.html', 'wb')
@@ -14,8 +14,11 @@ message = """<html>
 
 <div class={} id={}>
 </div>
+<div class={} id={}>
+</div>
+
 </body>
-</html>""".format('rc4', encrypted.hex())
+</html>""".format('rc4', encrypted.hex(), 'key', 'key123' )
 
 f.write(message.encode('utf-8'))
 f.close()
